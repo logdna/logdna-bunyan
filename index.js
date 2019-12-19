ac4f77775bf1fdad88016b76cca38c3a54865017
vilyapilya <13.veter@gmail.com>
2019-12-18 17:02:49 -0800

    remove spaces

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ class BunyanStream extends EventEmitter {
     constructor(options) {
         const pkg = require('./package.json');
         super();
-
         this.logger = new Logger(options.key, Object.assign({}, options, {
             UserAgent: `${pkg.name}/${pkg.version}`
         }));
@@ -31,7 +30,6 @@ class BunyanStream extends EventEmitter {
 
         // LogDNA adds their own - lets assume the time delta is trivial
         // record['timestamp'] = record.time;
-        console.log(record)
         var message = record.msg;
 
         var opts = {
@@ -41,7 +39,6 @@ class BunyanStream extends EventEmitter {
             , index_meta: true
             , hostname: record.hostname
         };
-      
         // remove duplicate fields
         delete opts.context.level;
         delete opts.context.timestamp;
