d5e0ac69318b088d49c5e9b283e02bd17b7aa790
Darin Spivey <darin.spivey@logdna.com>
2020-10-08 10:00:35 -0400

    deps: eslint-config-logdna@2.0.0
    Use our linting package rather than a local definition.
    
    Semver: patch
    Ref: LOG-7378

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-'use strict';
+'use strict'
 
-// External Modules
-const EventEmitter = require('events').EventEmitter;
-const Logger = require('logdna').Logger;
+const {EventEmitter} = require('events')
+const Logger = require('logdna').Logger
+const pkg = require('./package.json')
 
 // Constants
 const levels = {
-    10: 'TRACE'
-    , 20: 'DEBUG'
-    , 30: 'INFO'
-    , 40: 'WARN'
-    , 50: 'ERROR'
-    , 60: 'FATAL'
-};
+  10: 'TRACE'
+, 20: 'DEBUG'
+, 30: 'INFO'
+, 40: 'WARN'
+, 50: 'ERROR'
+, 60: 'FATAL'
+}
 
 /*
  *  Support for Bunyan Transport
  */
 class BunyanStream extends EventEmitter {
-    constructor(options) {
-        const pkg = require('./package.json');
-        super();
-        this.logger = new Logger(options.key, Object.assign({}, options, {
-            UserAgent: `${pkg.name}/${pkg.version}`
-        }));
+  constructor(options) {
+    super()
+    this.logger = new Logger(options.key, {
+      ...options
+    , UserAgent: `${pkg.name}/${pkg.version}`
+    })
+  }
+
+  write(record) {
+
+    // LogDNA adds their own - lets assume the time delta is trivial
+    // record['timestamp'] = record.time;
+
+    var message = record.msg
+
+    var opts = {
+      level: levels[record.level]
+    , app: record.name
+    , context: {...record}
+    , index_meta: true
+    , hostname: record.hostname
     }
+    // remove duplicate fields
+    delete opts.context.level
+    delete opts.context.timestamp
+    delete opts.context.name
+    delete opts.context.msg
 
-    write(record) {
-
-        // LogDNA adds their own - lets assume the time delta is trivial
-        // record['timestamp'] = record.time;
-
-        var message = record.msg;
-
-        var opts = {
-            level: levels[record.level]
-            , app: record.name
-            , context: Object.assign({}, record)
-            , index_meta: true
-            , hostname: record.hostname
-        };
-        // remove duplicate fields
-        delete opts.context.level;
-        delete opts.context.timestamp;
-        delete opts.context.name;
-        delete opts.context.msg;
-
-        try {
-            this.logger.log(message, opts);
-        } catch (err) {
-            this.emit('error', err);
-        }
-    };
+    try {
+      this.logger.log(message, opts)
+    } catch (err) {
+      this.emit('error', err)
+    }
+  };
 }
 
-module.exports.BunyanStream = BunyanStream;
+module.exports.BunyanStream = BunyanStream
